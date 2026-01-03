8a75736bd27f2d8f9d1b946b60e44d45d5c93542
Alexandre Freitas <8220154@ESTG.IPP.PT>
2026-01-03 15:39:23 +0000

    123

---
+++ Notifications/src/main/java/org/example/clients/UsersClient.java
@@ -0,0 +1,13 @@
+package org.example.clients;
+
+import org.example.dtos.UserDTO;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.UUID;
+
+@Component
+public class UsersClient {
+
+}
