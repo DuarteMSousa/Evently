b7c63bb41e0b6c22977fbf7a9a7d66404e66f322
paul0coelho <paulosmcoelho04@gmail.com>
2026-01-18 21:21:38 +0000

    Integrate Orders service via Feign client
    Added OpenFeign dependency and enabled Feign clients in the Payments application. Introduced OrdersClient interface and related DTOs/enums for external Orders service communication. Updated PaymentsService to validate payment amounts against order totals and handle external service exceptions.

---
+++ Payments/src/main/java/org/example/clients/OrdersClient.java
@@ -0,0 +1,17 @@
+package org.example.clients;
+
+import org.example.dtos.externalServices.OrderDTO;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.UUID;
+
+@FeignClient(name = "orders", path = "/orders")
+public interface OrdersClient {
+
+    @GetMapping("/get-order/{id}")
+    public ResponseEntity<OrderDTO> getOrder(@PathVariable("id") UUID id);
+
+}
